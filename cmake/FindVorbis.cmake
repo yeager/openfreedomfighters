0d744e5385b0d201966241b7dc1896174225b05b
Daniel Nylander <1206564+yeager@users.noreply.github.com>
2026-09-05 06:40:21 +0000

    Propagate Ogg headers through Vorbis targets

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -1,6 +1,7 @@
 include(FindPackageHandleStandardArgs)
 
 find_path(Vorbis_INCLUDE_DIR NAMES vorbis/vorbisfile.h vorbis/vorbisenc.h)
+find_path(Vorbis_OGG_INCLUDE_DIR NAMES ogg/ogg.h)
 find_library(Vorbis_FILE_LIBRARY NAMES vorbisfile libvorbisfile.so.3)
 find_library(Vorbis_ENCODER_LIBRARY NAMES vorbisenc libvorbisenc.so.2)
 find_library(Vorbis_LIBRARY NAMES vorbis libvorbis.so.0)
@@ -10,6 +11,7 @@ find_package_handle_standard_args(
     Vorbis
     REQUIRED_VARS
         Vorbis_INCLUDE_DIR
+        Vorbis_OGG_INCLUDE_DIR
         Vorbis_FILE_LIBRARY
         Vorbis_ENCODER_LIBRARY
         Vorbis_LIBRARY
@@ -21,7 +23,7 @@ if(Vorbis_FOUND AND NOT TARGET Vorbis::vorbisfile)
     set_target_properties(
         Vorbis::vorbisfile PROPERTIES
         IMPORTED_LOCATION "${Vorbis_FILE_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Vorbis_INCLUDE_DIR}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Vorbis_INCLUDE_DIR};${Vorbis_OGG_INCLUDE_DIR}"
         INTERFACE_LINK_LIBRARIES "${Vorbis_LIBRARY};${Vorbis_OGG_LIBRARY}"
     )
 endif()
@@ -31,13 +33,14 @@ if(Vorbis_FOUND AND NOT TARGET Vorbis::vorbisenc)
     set_target_properties(
         Vorbis::vorbisenc PROPERTIES
         IMPORTED_LOCATION "${Vorbis_ENCODER_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Vorbis_INCLUDE_DIR}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Vorbis_INCLUDE_DIR};${Vorbis_OGG_INCLUDE_DIR}"
         INTERFACE_LINK_LIBRARIES "${Vorbis_LIBRARY};${Vorbis_OGG_LIBRARY}"
     )
 endif()
 
 mark_as_advanced(
     Vorbis_INCLUDE_DIR
+    Vorbis_OGG_INCLUDE_DIR
     Vorbis_FILE_LIBRARY
     Vorbis_ENCODER_LIBRARY
     Vorbis_LIBRARY
